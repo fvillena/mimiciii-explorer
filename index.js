647da8d044475bfe874f0b77f274fe32aeb28b34
Fabián Villena <villenafabian@gmail.com>
2022-06-14 17:12:56 -0400

    sorting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     ENDPOINT = "http://grupo08.cc3201.dcc.uchile.cl"
-    $.get(`${ENDPOINT}/patients?select=subject_id`, function (data, status) {
+    $.get(`${ENDPOINT}/patients?select=subject_id&order=subject_id.asc`, function (data, status) {
         $.each(data, function (i, item) {
             $('#patients').append($('<option>', {
                 value: item.subject_id,
@@ -18,7 +18,7 @@ $(document).ready(function () {
             $("#patient-dob").text(data[0].dob);
             $("#patient-gender").text(data[0].gender);
         });
-        $.get(`${ENDPOINT}/icustays?subject_id=eq.${valueSelected}&select=icustay_id`, function (data, status) {
+        $.get(`${ENDPOINT}/icustays?subject_id=eq.${valueSelected}&select=icustay_id&order=icustay_id.asc`, function (data, status) {
             $('#icustays').empty().append('<option></option>');
             $.each(data, function (i, item) {
                 $('#icustays').append($('<option>', {
@@ -39,11 +39,11 @@ $(document).ready(function () {
             $("#patient-intime").text(data[0].intime);
             $("#patient-los").text(data[0].los);
         });
-        $.get(`${ENDPOINT}/chartevents_d_items?icustay_id=eq.${valueSelected}&valuenum=not.is.null&select=d_itemid,label`, function (data, status) {
+        $.get(`${ENDPOINT}/chartevents_d_items?icustay_id=eq.${valueSelected}&valuenum=not.is.null&select=d_itemid,label&order=label.asc`, function (data, status) {
             $('#items-form').show();
             let data_unique = [...new Map(data.map((item) => [item["d_itemid"], item])).values()];
+            $('#items').empty().append('<option></option>');
             $.each(data_unique, function (i, item) {
-                $('#items').empty().append('<option></option>');
                 $('#items').append($('<option>', {
                     value: item.d_itemid,
                     text: item.label
--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Lista de pacientes
 
-SELECT subject_id FROM mimiciii.patients;
--- GET /patients?select=subject_id
+SELECT subject_id FROM mimiciii.patients ORDER BY subject_id ASC;
+-- GET /patients?select=subject_id&order=subject_id.asc
 
 -- Información del paciente
 
@@ -10,8 +10,8 @@ SELECT * FROM mimiciii.patients WHERE subject_id=10019;
 
 -- Hospitalizaciones en UCI del paciente
 
-SELECT icustay_id FROM mimiciii.icustays WHERE subject_id=10019;
--- GET /icustays?subject_id=eq.10019&select=icustay_id
+SELECT icustay_id FROM mimiciii.icustays WHERE subject_id=10019 ORDER BY icustay_id ASC;
+-- GET /icustays?subject_id=eq.10019&select=icustay_id&order=icustay_id.asc
 
 -- Información de la hospitalización en UCI
 
@@ -20,8 +20,8 @@ SELECT * FROM mimiciii.icustays WHERE icustay_id=228977;
 
 -- Parámetros medidos disponibles en la hospitalización en UCI
 
-SELECT DISTINCT d_itemid,label FROM mimiciii.chartevents_d_items WHERE icustay_id=228977 AND valuenum IS NOT NULL;
--- GET /chartevents_d_items?icustay_id=eq.228977&valuenum=not.is.null&select=d_itemid,label
+SELECT DISTINCT d_itemid,label FROM mimiciii.chartevents_d_items WHERE icustay_id=228977 AND valuenum IS NOT NULL ORDER BY label ASC;
+-- GET /chartevents_d_items?icustay_id=eq.228977&valuenum=not.is.null&select=d_itemid,label&order=label.asc
 
 -- Mediciones del parámetro en la hospitalización en UCI
 
